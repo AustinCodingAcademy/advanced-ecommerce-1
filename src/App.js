e7faf81a56e4ce021fa8a7754b54736f9da6f7ec
Jonathan Gucciardi <jonmgucciardi@Jonathans-MacBook-Pro.local>
2017-04-18 11:33:02 -0500

    homework 1 test commit

--- src/App.js
+++ src/App.js
@@ -5,10 +5,10 @@ import './App.css';
 function App() {
     return (
       <div className="App">
-     
+
     <nav className="navbar navbar-inverse navbar-fixed-top" role="navigation">
         <div className="container">
-          
+
             <div className="navbar-header">
                 <button type="button" className="navbar-toggle" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1">
                     <span className="sr-only">Toggle navigation</span>
@@ -18,7 +18,7 @@ function App() {
                 </button>
                 <a className="navbar-brand" href="#">Start Bootstrap</a>
             </div>
-          
+
             <div className="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
                 <ul className="nav navbar-nav">
                     <li>
@@ -32,12 +32,12 @@ function App() {
                     </li>
                 </ul>
             </div>
-         
+
         </div>
-      
+
     </nav>
 
- 
+
     <div className="container">
 
         <div className="row">
@@ -210,12 +210,12 @@ function App() {
         </div>
 
     </div>
-   
+
     <div className="container">
 
         <hr/>
 
-      
+
         <footer>
             <div className="row">
                 <div className="col-lg-12">
