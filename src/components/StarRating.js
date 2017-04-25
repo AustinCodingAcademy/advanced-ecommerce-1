7ad5173fcc02055c83f21e66e02f47471c457328
Tony <arinaldi@vt.edu>
2017-04-24 21:28:49 -0500

    Convert star rating to for loop

--- src/components/StarRating.js
+++ src/components/StarRating.js
@@ -2,10 +2,11 @@ import React from 'react';
 
 function StarRating(props) {
   const { rating } = props;
-  const arr = Array(rating).fill(null);
-  const starSpans = arr.map((item, index) => {
-    return <span key={index} className="glyphicon glyphicon-star"></span>;
-  });
+  const starSpans = [];
+  
+  for (var i = 1; i <= rating; i++) {
+    starSpans.push(<span key={i} className="glyphicon glyphicon-star"></span>)
+  }
   
   return (
     <p>{starSpans}</p>
