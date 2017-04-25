a378c6db094b66cc8642a111e408d16991d4badf
aleexga <aleexga@gmail.com>
2017-04-24 23:40:56 -0500

    App.js does not load info!
    -App.js accesses state but doesn’t bring in anything.

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -3,7 +3,16 @@ import "./App.css";
 
 function Footer() {
   return (
-    <p>Footer!</p>
+    <div className="container">
+      <hr />
+      <footer>
+        <div className="row">
+          <div className="col-lg-12">
+            <p>Copyright &copy; Your Website 2014</p>
+          </div>
+        </div>
+      </footer>
+    </div>
   );
 }
 
--- src/components/ProductDetail.js
+++ src/components/ProductDetail.js
@@ -1,43 +1,33 @@
 import React from "react";
 import "./App.css";
-import state from "./state";
 
-
-function ProductDetail() {
-
-  function showProductDetail(product) {
-    return (
-      <div className="col-sm-4 col-lg-4 col-md-4">
-        <div className="thumbnail">
-          <img src="{product.imgUrl}" alt="" />
-          <div className="caption">
-            <h4 className="pull-right">
-              {products.price}
-            </h4>
-            <h4>
-              <a href="#">{product.name}</a>
-            </h4>
-            <p>{product.description}<a target="_blank" href="http://www.bootsnipp.com">Bootsnipp - http://bootsnipp.com</a>.</p>
-          </div>
-          <div className="ratings">
-            <p className="pull-right">{product.reviews} reviews</p>
-            <p>
-              <span className="glyphicon glyphicon-star" />
-              <span className="glyphicon glyphicon-star" />
-              <span className="glyphicon glyphicon-star" />
-              <span className="glyphicon glyphicon-star" />
-              <span className="glyphicon glyphicon-star" />
-            </p>
-          </div>
+function ProductDetail(product) {
+  return (
+    <div className="col-sm-4 col-lg-4 col-md-4">
+      <div className="thumbnail">
+        <img src="{product.imgUrl}" alt="" />
+        <div className="caption">
+          <h4 className="pull-right">
+            {product.price}
+          </h4>
+          <h4>
+            <a href="#">{product.name}</a>
+          </h4>
+          <p>{product.description}<a target="_blank" href="http://www.bootsnipp.com">Bootsnipp - http://bootsnipp.com</a>.</p>
+        </div>
+        <div className="ratings">
+          <p className="pull-right">{product.reviews} reviews</p>
+          <p>
+            <span className="glyphicon glyphicon-star" />
+            <span className="glyphicon glyphicon-star" />
+            <span className="glyphicon glyphicon-star" />
+            <span className="glyphicon glyphicon-star" />
+            <span className="glyphicon glyphicon-star" />
+          </p>
         </div>
       </div>
-    );
-  }
-  state.products.map((product) => {
-    return (
-      <showProductDetail key={product.id} imgUrl={product.imgUrl} />
-    );
-  });
+    </div>
+  );
 }
 
 export default ProductDetail;
