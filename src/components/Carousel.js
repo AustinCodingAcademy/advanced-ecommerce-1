0e36e4c9fa345f0a812bfe05aa5430b7e9d61439
Leslie Robertson <leslievrobertson@gmail.com>
2017-04-25 18:10:52 -0500

    Added modules, added star function

---
+++ src/components/Carousel.js
@@ -0,0 +1,43 @@
+import React from "react";
+
+function Carousel() {
+  return (
+    <div className="col-md-9">
+      <div className="row carousel-holder">
+        <div className="col-md-12">
+          <div id="carousel-example-generic" className="carousel slide" data-ride="carousel">
+
+            <ol className="carousel-indicators">
+              <li data-target="#carousel-example-generic" data-slide-to="0" className="active" />
+              <li data-target="#carousel-example-generic" data-slide-to="1" />
+              <li data-target="#carousel-example-generic" data-slide-to="2" />
+            </ol>
+
+            <div className="carousel-inner">
+              <div className="item active">
+                <img className="slide-image" src="http://placehold.it/800x300" alt="" />
+              </div>
+
+              <div className="item">
+                <img className="slide-image" src="http://placehold.it/800x300" alt="" />
+              </div>
+
+              <div className="item">
+                <img className="slide-image" src="http://placehold.it/800x300" alt="" />
+              </div>
+            </div>
+
+            <a className="left carousel-control" href="#carousel-example-generic" data-slide="prev">
+              <span className="glyphicon glyphicon-chevron-left" />
+            </a>
+            <a className="right carousel-control" href="#carousel-example-generic" data-slide="next">
+              <span className="glyphicon glyphicon-chevron-right" />
+            </a>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default Carousel;
---
+++ src/components/Categories.js
@@ -0,0 +1,16 @@
+import React from "react";
+
+function Categories() {
+  return (
+    <div className="col-md-3">
+      <p className="lead">Shop Name</p>
+      <div className="list-group">
+        <a href="#" className="list-group-item">Category 1</a>
+        <a href="#" className="list-group-item">Category 2</a>
+        <a href="#" className="list-group-item">Category 3</a>
+      </div>
+    </div>
+  );
+}
+
+export default Categories;
