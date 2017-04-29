cf630efdbae4ca01a7e9630a3cfed802a4b57bbd
Collind09 <collind09@gmail.com>
2017-04-28 22:38:34 -0500

    Homework 1 Working

--- src/components/ProductDetail.js
+++ src/components/ProductDetail.js
@@ -6,7 +6,18 @@ function ProductDetail(props) {
   console.log(product);
 
   return (
-    <div>{product.name}  {product.reviews} {product.description} {product.rating} {product.id} </div>
+    <div className="col-sm-4 col-lg-4 col-md-4">
+      <div className= "thumbnail">
+        <img src = {product.imgUrl} alt ={product.id}/> 
+      <div className= "caption">
+        <h4 className="pull-right">{product.price}</h4>
+        <h4><a href="#">{product.name}</a></h4>
+        <p>{product.description}</p>
+        </div>
+        <p className="pull-right">{product.reviews}</p> 
+      {product.rating}  
+      </div>
+      </div>
   );
 };
 
