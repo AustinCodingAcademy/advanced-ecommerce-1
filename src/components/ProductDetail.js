a63ff6bc39f870c874db42d37632cff5bf2dfebc
joeycaradonna <joeycaradonna45@gmail.com>
2017-04-30 17:44:47 -0500

    Homework 1 finished

--- src/components/ProductDetail.js
+++ src/components/ProductDetail.js
@@ -1,12 +1,24 @@
-import React, { Component } from "react";
+import React from "react";
+import StarRating from "./StarRating";
 
 function ProductDetail(props) {
-    return(
-        <div className="caption">
-                                <h4 className="pull-right">$64.99</h4>
-                                <h4><a href="#">Second Product</a>
-                                </h4>
-                                <p>This is a short description. Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
-                            </div>
-    )
-}
+  const product = props.product;
+  console.log(product);
+
+  return (
+    <div className="col-sm-4 col-lg-4 col-md-4">
+      <div className= "thumbnail">
+        <img src = {product.imgUrl} alt ={product.id}/>
+      <div className= "caption">
+        <h4 className="pull-right">{product.price}</h4>
+        <h4><a href="#">{product.name}</a></h4>
+        <p>{product.description}</p>
+        </div>
+        <p className="pull-right">{product.reviews}</p>
+      {product.rating}
+      </div>
+      </div>
+  );
+};
+
+export default ProductDetail;
