086b814d30c71b4a4f2a327d2215629048a8b045
stackbaxter <stackbaxter@yahoo.com>
2017-07-25 13:02:17 -0500

    about to map

--- src/components/ProductDetail.js
+++ src/components/ProductDetail.js
@@ -3,21 +3,21 @@ import React from 'react';
 
 export default function ProductDetail(props){
 
-  var products = props.products;
+  var products = props.products.products;
     console.log(products);
   return(
   <div>
     <div className="col-sm-4 col-lg-4 col-md-4">
         <div className="thumbnail">
 
-            <img src={products.products[0].imgUrl} alt=""/>
+            <img src={products[0].imgUrl} alt=""/>
             <div className="caption">
-                <h4 className="pull-right">$24.99</h4>
-                <h4><a href="#">{products.products[0].name}</a>
+                <h4 className="pull-right">{}</h4>
+                <h4><a href="#">{products[0].name}</a>
                 </h4>
-                <p>{products.products[0].description}<a target="_blank" href="http://www.bootsnipp.com">Bootsnipp - http://bootsnipp.com</a>.</p>
+                <p>{products[0].description}<a target="_blank" href="http://www.bootsnipp.com">Bootsnipp - http://bootsnipp.com</a>.</p>
             </div>
-                <p className="pull-right">{products.products[0].reviews}reviews</p>
+                <p className="pull-right">{products[0].price}reviews</p>
                 <p>
                     <span className="glyphicon glyphicon-star"></span>
                     <span className="glyphicon glyphicon-star"></span>
