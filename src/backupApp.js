ef4b02d5da11513bb5e2c5878dc30b93131a0dc2
stackbaxter <stackbaxter@yahoo.com>
2017-07-25 03:18:48 -0500

    prod deets left

--- src/backupApp.js
+++ src/backupApp.js
@@ -1,30 +1,112 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
-import HeaderNav from './components/HeaderNav';
-import SideBar from './components/SideBar';
-import Carousel from './components/Carousel';
-import ProductDetail from './components/ProductDetail';
 
-function App(props) {
-  console.log(props.products)
+function App() {
     return (
       <div className="App">
+     
+    <nav className="navbar navbar-inverse navbar-fixed-top" role="navigation">
+        <div className="container">
+          
+            <div className="navbar-header">
+                <button type="button" className="navbar-toggle" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1">
+                    <span className="sr-only">Toggle navigation</span>
+                    <span className="icon-bar"></span>
+                    <span className="icon-bar"></span>
+                    <span className="icon-bar"></span>
+                </button>
+                <a className="navbar-brand" href="#">Start Bootstrap</a>
+            </div>
+          
+            <div className="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
+                <ul className="nav navbar-nav">
+                    <li>
+                        <a href="#">About</a>
+                    </li>
+                    <li>
+                        <a href="#">Services</a>
+                    </li>
+                    <li>
+                        <a href="#">Contact</a>
+                    </li>
+                </ul>
+            </div>
+         
+        </div>
+      
+    </nav>
 
-    <HeaderNav />
-
+ 
     <div className="container">
 
         <div className="row">
 
-            <SideBar />
+            <div className="col-md-3">
+                <p className="lead">Shop Name</p>
+                <div className="list-group">
+                    <a href="#" className="list-group-item">Category 1</a>
+                    <a href="#" className="list-group-item">Category 2</a>
+                    <a href="#" className="list-group-item">Category 3</a>
+                </div>
+            </div>
 
             <div className="col-md-9">
-              <Carousel />
+
+                <div className="row carousel-holder">
+
+                    <div className="col-md-12">
+                        <div id="carousel-example-generic" className="carousel slide" data-ride="carousel">
+                            <ol className="carousel-indicators">
+                                <li data-target="#carousel-example-generic" data-slide-to="0" className="active"></li>
+                                <li data-target="#carousel-example-generic" data-slide-to="1"></li>
+                                <li data-target="#carousel-example-generic" data-slide-to="2"></li>
+                            </ol>
+                            <div className="carousel-inner">
+                                <div className="item active">
+                                    <img className="slide-image" src="http://placehold.it/800x300" alt=""/>
+                                </div>
+                                <div className="item">
+                                    <img className="slide-image" src="http://placehold.it/800x300" alt=""/>
+                                </div>
+                                <div className="item">
+                                    <img className="slide-image" src="http://placehold.it/800x300" alt=""/>
+                                </div>
+                            </div>
+                            <a className="left carousel-control" href="#carousel-example-generic" data-slide="prev">
+                                <span className="glyphicon glyphicon-chevron-left"></span>
+                            </a>
+                            <a className="right carousel-control" href="#carousel-example-generic" data-slide="next">
+                                <span className="glyphicon glyphicon-chevron-right"></span>
+                            </a>
+                        </div>
+                    </div>
+
+                </div>
 
                 <div className="row">
-                      <ProductDetail products={props.products} />
 
+                    <div className="col-sm-4 col-lg-4 col-md-4">
+                        <div className="thumbnail">
+                            <img src="http://placehold.it/320x150" alt=""/>
+                            <div className="caption">
+                                <h4 className="pull-right">$24.99</h4>
+                                <h4><a href="#">First Product</a>
+                                </h4>
+                                <p>See more snippets like this online store item at <a target="_blank" href="http://www.bootsnipp.com">Bootsnipp - http://bootsnipp.com</a>.</p>
+                            </div>
+                            <div className="ratings">
+                                <p className="pull-right">15 reviews</p>
+                                <p>
+                                    <span className="glyphicon glyphicon-star"></span>
+                                    <span className="glyphicon glyphicon-star"></span>
+                                    <span className="glyphicon glyphicon-star"></span>
+                                    <span className="glyphicon glyphicon-star"></span>
+                                    <span className="glyphicon glyphicon-star"></span>
+                                </p>
+                            </div>
+                        </div>
+                    </div>
 
                     <div className="col-sm-4 col-lg-4 col-md-4">
                         <div className="thumbnail">
@@ -128,12 +210,12 @@ function App(props) {
         </div>
 
     </div>
-
+   
     <div className="container">
 
         <hr/>
 
-
+      
         <footer>
             <div className="row">
                 <div className="col-lg-12">
---
+++ src/components/StarRating.js
@@ -0,0 +1,58 @@
+import React from 'react';
+
+export default class StarRating extends React.Component {
+  constructor() {
+    super();
+
+    this.state = {
+      rating: 1,
+      rating_custom_icon: 6,
+      rating_half_star: 3.5,
+      rating_empty_initial: 0
+    };
+  }
+
+  onStarClick(nextValue, prevValue, name) {
+    console.log('name: %s, nextValue: %s, prevValue: %s', name, nextValue, prevValue);
+    this.setState({rating: nextValue});
+  }
+
+  onStarClickCustomIcon(nextValue, prevValue, name) {
+    console.log('name: %s, nextValue: %s, prevValue: %s', name, nextValue, prevValue);
+    this.setState({rating_custom_icon: nextValue});
+  }
+
+  onStarClickHalfStar(nextValue, prevValue, name) {
+    console.log('name: %s, nextValue: %s, prevValue: %s', name, nextValue, prevValue);
+    this.setState({rating_half_star: nextValue});
+  }
+
+  onStarClickEmptyInitial(nextValue, prevValue, name) {
+    console.log('name: %s, nextValue: %s, prevValue: %s', name, nextValue, prevValue);
+    this.setState({rating_empty_initial: nextValue});
+  }
+
+  render() {
+    return (
+
+        <div className="ratings">
+
+
+           <div style={{fontSize: 24}}>
+
+             <StarRating
+               name="app5"
+               starColor="#ffb400"
+               emptyStarColor="#ffb400"
+               value={this.state.rating_half_star}
+               onStarClick={this.onStarClickHalfStar.bind(this)}
+               renderStarIcon={(index, value) => {
+                 return <span className={index <= value ? 'fa fa-star' : 'fa fa-star-o'} />;
+               }}
+               renderStarIconHalf={() => <span className="fa fa-star-half-full" />}
+             />
+           </div>
+         </div>
+       )
+     }
+   }
