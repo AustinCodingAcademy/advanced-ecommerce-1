cee68c623577d8779864314838e275f844c812b8
Juan Mata <juanmata@Juans-MacBook-Pro.local>
2017-07-26 20:39:52 -0500

    initial

--- src/App.js
+++ src/App.js
@@ -1,14 +1,22 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import ProcutDetail from "./components/ProductDetail"
 
-function App() {
+function App(props) {
+
+    var productDiv = props.products.map((p) => {
+      return <ProductDetail product={p} />
+    });
+
+
     return (
       <div className="App">
-     
+
+
     <nav className="navbar navbar-inverse navbar-fixed-top" role="navigation">
         <div className="container">
-          
+
             <div className="navbar-header">
                 <button type="button" className="navbar-toggle" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1">
                     <span className="sr-only">Toggle navigation</span>
@@ -18,7 +26,7 @@ function App() {
                 </button>
                 <a className="navbar-brand" href="#">Start Bootstrap</a>
             </div>
-          
+
             <div className="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
                 <ul className="nav navbar-nav">
                     <li>
@@ -32,12 +40,12 @@ function App() {
                     </li>
                 </ul>
             </div>
-         
+
         </div>
-      
+
     </nav>
 
- 
+
     <div className="container">
 
         <div className="row">
@@ -210,12 +218,12 @@ function App() {
         </div>
 
     </div>
-   
+
     <div className="container">
 
         <hr/>
 
-      
+
         <footer>
             <div className="row">
                 <div className="col-lg-12">
