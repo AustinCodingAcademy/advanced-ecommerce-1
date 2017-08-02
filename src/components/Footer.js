d197631730edda069d9899927db771f0d5133d48
Ericka Emery <ericka@elonandcompany.com>
2017-08-02 06:33:40 -0500

    components made & copied

---
+++ src/components/Footer.js
@@ -0,0 +1,17 @@
+import React from 'react';
+
+function Footer () {
+  return (
+    <div>
+    <footer>
+        <div className="row">
+            <div className="col-lg-12">
+                <p>Copyright &copy; Your Website 2014</p>
+            </div>
+        </div>
+    </footer>
+    </div>
+  );
+}
+
+export default Footer;
