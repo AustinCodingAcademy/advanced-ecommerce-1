732f0af78d72b36596305a72894c9524a9ccffef
Greg Swan <greg.c.swan@gmail.com>
2017-10-09 20:54:04 -0500

    made elements into components

--- src/App.js
+++ src/App.js
@@ -1,6 +1,10 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import Header from './components/Header';
+import ProductDetail from './components/ProductDetail';
+import Footer from './components/Footer';
+import Carousel from './components/Carousel'
 
 function App() {
     return (
@@ -8,36 +12,10 @@ function App() {
      
     <nav className="navbar navbar-inverse navbar-fixed-top" role="navigation">
         <div className="container">
-          
-            <div className="navbar-header">
-                <button type="button" className="navbar-toggle" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1">
-                    <span className="sr-only">Toggle navigation</span>
-                    <span className="icon-bar"></span>
-                    <span className="icon-bar"></span>
-                    <span className="icon-bar"></span>
-                </button>
-                <a className="navbar-brand" href="#">Start Bootstrap</a>
-            </div>
-          
-            <div className="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
-                <ul className="nav navbar-nav">
-                    <li>
-                        <a href="#">About</a>
-                    </li>
-                    <li>
-                        <a href="#">Services</a>
-                    </li>
-                    <li>
-                        <a href="#">Contact</a>
-                    </li>
-                </ul>
-            </div>
-         
+            <Header />
         </div>
-      
     </nav>
 
- 
     <div className="container">
 
         <div className="row">
@@ -52,61 +30,13 @@ function App() {
             </div>
 
             <div className="col-md-9">
-
                 <div className="row carousel-holder">
-
-                    <div className="col-md-12">
-                        <div id="carousel-example-generic" className="carousel slide" data-ride="carousel">
-                            <ol className="carousel-indicators">
-                                <li data-target="#carousel-example-generic" data-slide-to="0" className="active"></li>
-                                <li data-target="#carousel-example-generic" data-slide-to="1"></li>
-                                <li data-target="#carousel-example-generic" data-slide-to="2"></li>
-                            </ol>
-                            <div className="carousel-inner">
-                                <div className="item active">
-                                    <img className="slide-image" src="http://placehold.it/800x300" alt=""/>
-                                </div>
-                                <div className="item">
-                                    <img className="slide-image" src="http://placehold.it/800x300" alt=""/>
-                                </div>
-                                <div className="item">
-                                    <img className="slide-image" src="http://placehold.it/800x300" alt=""/>
-                                </div>
-                            </div>
-                            <a className="left carousel-control" href="#carousel-example-generic" data-slide="prev">
-                                <span className="glyphicon glyphicon-chevron-left"></span>
-                            </a>
-                            <a className="right carousel-control" href="#carousel-example-generic" data-slide="next">
-                                <span className="glyphicon glyphicon-chevron-right"></span>
-                            </a>
-                        </div>
-                    </div>
-
+                    <Carousel />
                 </div>
 
                 <div className="row">
-
-                    <div className="col-sm-4 col-lg-4 col-md-4">
-                        <div className="thumbnail">
-                            <img src="http://placehold.it/320x150" alt=""/>
-                            <div className="caption">
-                                <h4 className="pull-right">$24.99</h4>
-                                <h4><a href="#">First Product</a>
-                                </h4>
-                                <p>See more snippets like this online store item at <a target="_blank" href="http://www.bootsnipp.com">Bootsnipp - http://bootsnipp.com</a>.</p>
-                            </div>
-                            <div className="ratings">
-                                <p className="pull-right">15 reviews</p>
-                                <p>
-                                    <span className="glyphicon glyphicon-star"></span>
-                                    <span className="glyphicon glyphicon-star"></span>
-                                    <span className="glyphicon glyphicon-star"></span>
-                                    <span className="glyphicon glyphicon-star"></span>
-                                    <span className="glyphicon glyphicon-star"></span>
-                                </p>
-                            </div>
-                        </div>
-                    </div>
+                    <ProductDetail />
+                    
 
                     <div className="col-sm-4 col-lg-4 col-md-4">
                         <div className="thumbnail">
@@ -212,18 +142,8 @@ function App() {
     </div>
    
     <div className="container">
-
         <hr/>
-
-      
-        <footer>
-            <div className="row">
-                <div className="col-lg-12">
-                    <p>Copyright &copy; Your Website 2014</p>
-                </div>
-            </div>
-        </footer>
-
+        <Footer />
     </div>
       </div>
     );
