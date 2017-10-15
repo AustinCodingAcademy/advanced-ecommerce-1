fd752b68772a62e0b3e18083c9b2441cc52298aa
CruzMendez <camendez@utexas.edu>
2017-10-15 12:19:55 -0500

    updated Homework 1

---
+++ src/components/ProductDetail.js
@@ -0,0 +1,30 @@
+import React from "react";
+
+function ProductDetail(props) {
+
+    return (
+        <div className="col-sm-4 col-lg-4 col-md-4">
+            <div className="thumbnail">
+                <img src={props.products.imgUrl} alt=""/>
+                <div className="caption">
+                    <h4 className="pull-right">{props.products.price}</h4>
+                    <h4><a href="#">{props.products.id}</a>
+                    </h4>
+                    <p>{props.products.description}</p>
+                </div>
+                <div className="ratings">
+                    <p className="pull-right">{props.products.reviews}</p>
+                    <p>
+                        <span className="glyphicon glyphicon-star"></span>
+                        <span className="glyphicon glyphicon-star"></span>
+                        <span className="glyphicon glyphicon-star"></span>
+                        <span className="glyphicon glyphicon-star"></span>
+                        <span className="glyphicon glyphicon-star-empty"></span>
+                    </p>
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default ProductDetail;
