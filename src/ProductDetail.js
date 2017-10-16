c72ea0fb9b0e642e2cdd13e3cc67b13912ca5a0e
Adrian Krach <mbairak@Adrians-MacBook-Air.local>
2017-10-16 18:27:56 -0500

    typo2

--- src/ProductDetail.js
+++ src/ProductDetail.js
@@ -14,7 +14,7 @@ function ProductDetail(props){
   return (
     <div className="col-sm-4 col-lg-4 col-md-4">
         <div className="thumbnail">
-            <img src={props.products.imgUrl} alt=""/>
+            <img src={props.product.imgUrl} alt=""/>
             <div className="caption">
                 <h4 className="pull-right">{props.product.price}</h4>
                 <h4><a href="#">{props.product.name}</a>
