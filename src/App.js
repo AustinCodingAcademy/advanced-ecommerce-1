d0ca3f6bb6667f4b58c1fb98bf596bb62c0463de
Renata Estes <renataestes@Renatas-MacBook-Air.local>
2017-10-16 18:25:25 -0500

    App.js changes

--- src/App.js
+++ src/App.js
@@ -87,13 +87,8 @@ function App(props) {
         <hr/>
 
 
-        <footer>
-            <div className="row">
-                <div className="col-lg-12">
-                    <p>Copyright &copy; Your Website 2014</p>
-                </div>
-            </div>
-        </footer>
+      <Footer />
+            
 
     </div>
       </div>
