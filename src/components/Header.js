e9e5599753572e87bc56aadb9bbfc9fc72f7ba29
Renata Estes <renataestes@Renatas-MacBook-Air.local>
2017-10-16 02:03:15 -0500

    first commit

---
+++ src/components/Header.js
@@ -0,0 +1,17 @@
+import React from "react";
+
+export default function Header(props){
+
+  return (
+
+<div className="navbar-header">
+    <button type="button" className="navbar-toggle" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1">
+        <span className="sr-only">Toggle navigation</span>
+        <span className="icon-bar"></span>
+        <span className="icon-bar"></span>
+        <span className="icon-bar"></span>
+    </button>
+    <a className="navbar-brand" href="#">Start Bootstrap</a>
+</div>
+);
+}
