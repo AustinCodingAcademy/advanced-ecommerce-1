b71fb277e471a8082b87fc13282e55c7fc1835a2
cfissell50 <cfissell@hotmail.com>
2018-01-22 20:31:08 -0600

    yooo

--- src/App.js
+++ src/App.js
@@ -1,24 +1,17 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import Header from './Header';
 
 function App() {
     return (
       <div className="App">
-     
+
     <nav className="navbar navbar-inverse navbar-fixed-top" role="navigation">
         <div className="container">
-          
-            <div className="navbar-header">
-                <button type="button" className="navbar-toggle" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1">
-                    <span className="sr-only">Toggle navigation</span>
-                    <span className="icon-bar"></span>
-                    <span className="icon-bar"></span>
-                    <span className="icon-bar"></span>
-                </button>
-                <a className="navbar-brand" href="#">Start Bootstrap</a>
-            </div>
-          
+
+        <Header />
+
             <div className="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
                 <ul className="nav navbar-nav">
                     <li>
@@ -32,12 +25,12 @@ function App() {
                     </li>
                 </ul>
             </div>
-         
+
         </div>
-      
+
     </nav>
 
- 
+
     <div className="container">
 
         <div className="row">
@@ -210,12 +203,12 @@ function App() {
         </div>
 
     </div>
-   
+
     <div className="container">
 
         <hr/>
 
-      
+
         <footer>
             <div className="row">
                 <div className="col-lg-12">
