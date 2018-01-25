9fb63f0e7c96f11e70e2ac34e450ffaa6822a304
Aaron Austin <aaronaustin@Aarons-MacBook-Pro.local>
2018-01-24 21:26:31 -0600

    Commit for hw

---
+++ src/component/ProductDetail.js
@@ -0,0 +1,32 @@
+import React from 'react'; 
+function ProductDetail(props){
+    return ( 
+        // {props.product.name}
+                    <div className="col-sm-4 col-lg-4 col-md-4">
+                    <div className="thumbnail">
+                        {/* <img src="http://placehold.it/320x150" alt=""/> */}
+                        <img src={props.product.imgUrl} alt=""/>
+
+                        <div className="caption">
+                            <h4 className="pull-right">{props.product.price}</h4>
+                            <h4><a href="#">{props.product.name} </a>
+                            </h4>
+                            <p> {props.product.description} </p>
+                            {/* <p>See more snippets like this online store item at <a target="_blank" href="http://www.bootsnipp.com">Bootsnipp - http://bootsnipp.com</a>.</p> */}
+                        </div>
+                        <div className="ratings">
+                            <p> {props.product.rating} </p>
+                            <p className="pull-right">{props.product.reviews} Reviews</p>
+                            <p>
+                                <span className="glyphicon glyphicon-star"></span>
+                                <span className="glyphicon glyphicon-star"></span>
+                                <span className="glyphicon glyphicon-star"></span>
+                                <span className="glyphicon glyphicon-star"></span>
+                                <span className="glyphicon glyphicon-star"></span>
+                            </p>
+                        </div>
+                    </div>
+                </div>
+        )      
+    }
+export default ProductDetail;
