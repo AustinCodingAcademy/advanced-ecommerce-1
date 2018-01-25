278e540c64391314d6445f0598d7c00df90fa3e0
cfissell50 <cfissell@hotmail.com>
2018-01-24 18:34:22 -0600

    yooo

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import './App.css';
 import Header from './ComponentFolder/Header';
 import Footer from './ComponentFolder/Footer';
 import Carousel from './ComponentFolder/Carousel';
+import ProductDetail from './ComponentFolder/ProductDetail';
 
 function App() {
     return (
@@ -168,6 +169,8 @@ function App() {
                         <p>If you like this template, then check out <a target="_blank" href="http://maxoffsky.com/code-blog/laravel-shop-tutorial-1-building-a-review-system/">this tutorial</a> on how to build a working review system for your online store!</p>
                         <a className="btn btn-primary" target="_blank" href="http://maxoffsky.com/code-blog/laravel-shop-tutorial-1-building-a-review-system/">View Tutorial</a>
                     </div>
+
+                    <ProductDetail />
 
                 </div>
 
