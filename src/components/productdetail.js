a2df014cde41e80a221eec72e89878ac4fd7033e
Deandre Crayton <deandrecrayton@gmail.com>
2018-01-28 16:21:36 -0600

    added dummy images

--- src/components/productdetail.js
+++ src/components/productdetail.js
@@ -5,7 +5,7 @@ function ProductDetail(props) {
     return(
         <div className="col-sm-4 col-lg-4 col-md-4">
         <div className="thumbnail">
-            <img src="http://placehold.it/320x150" alt=""/>
+            <img src={props.product.imgUrl} alt=""/>
             <div className="caption">
                 <h4 className="pull-right">{props.product.price}</h4>
                 <h4><a href="#">{props.product.name}</a>
