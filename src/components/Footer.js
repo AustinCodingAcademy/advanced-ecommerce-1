aafcec447d43041e86cafc8cc933c15ff1181d21
stephsingler <singlerstephanie@gmail.com>
2018-01-29 07:37:14 -0600

    minor changes

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -2,13 +2,11 @@ import React from "react";
 
 const Footer = () => {
   return (
-    <footer>
         <div className="row">
-            <div className="col-lg-12">
+            <div className="col-lg-12 footer">
                 <p>Copyright &copy; 2018 Stephanie's Wine</p>
             </div>
         </div>
-    </footer>
   );
 }
 export default Footer;
