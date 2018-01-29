74e736301549a75714201401e67d22ceece56f2e
cfissell50 <cfissell@hotmail.com>
2018-01-29 15:09:40 -0600

    getting there

--- src/ComponentFolder/ProductDetail.js
+++ src/ComponentFolder/ProductDetail.js
@@ -1,28 +1,38 @@
 import React from "react";
-import "../state.js";
 
 function ProductDetail(props){
+
+  const starSystem = () => {
+  const argument = props.products.rating;
+  let arr = [];
+  for (let i = 0; i < argument; i++) {
+    arr.push(<span className="glyphicon glyphicon-star" key={i} ></span>);
+  }
+  return arr;
+};
 
   return(
     <div className="col-sm-4 col-lg-4 col-md-4">
       <div className="thumbnail">
-          <img src="http://placehold.it/320x150" alt=""/>
-          <div className="caption">
-              <h4 className="pull-right">$24.99</h4>
-              <h4><a href="#">First Product</a>
-              </h4>
-              <p>See more snippets like this online store item at <a target="_blank" href="http://www.bootsnipp.com">Bootsnipp - http://bootsnipp.com</a>.</p>
+        <img src={props.products.imgUrl} alt=""/>
+        <div className="caption">
+          <h4 className="pull-right">{props.products.price}</h4>
+          <h4><a href="#">{props.products.name}</a>
+          </h4>
+          <p>{props.products.description} <a target="_blank" href="http://www.bootsnipp.com">Bootsnipp-http://bootsnipp.com</a>
+          </p>
+          <div>
+
           </div>
-          <div className="ratings">
-              <p className="pull-right">15 reviews</p>
-              <p>
-                  <span className="glyphicon glyphicon-star"></span>
-                  <span className="glyphicon glyphicon-star"></span>
-                  <span className="glyphicon glyphicon-star"></span>
-                  <span className="glyphicon glyphicon-star"></span>
-                  <span className="glyphicon glyphicon-star"></span>
-              </p>
-          </div>
+
+        </div>
+        <div className="ratings">
+          <p className="pull-right">{props.products.reviews}</p>
+          <p>
+            {/*<button id="addToCart">Add To Cart</button>*/}
+            {starSystem}
+          </p>
+        </div>
       </div>
     </div>
   )
