84ca25a7751ab3d0143d7742d56a4b8a0d7c8bf4
mimitch2 <mike@melodix.com>
2018-05-21 08:37:57 -0500

    first push, got product populating

--- src/index.js
+++ src/index.js
@@ -1,10 +1,12 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import App from './App';
-import './index.css';
-import state from './state';
+import React from "react";
+import ReactDOM from "react-dom";
+import App from "./App";
+import "./index.css";
+import state from "./state";
+
+
 
 ReactDOM.render(
-  <App />,
-  document.getElementById('root')
+  <App products={state.products} />,
+  document.getElementById("root")
 );
