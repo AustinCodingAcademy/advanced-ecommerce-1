3bd7f224430c25e6bda67da99c76b156e51ad4bd
Shelby Inloes <shelbyinloes@gmail.com>
2018-05-28 21:22:10 -0500

    dropdown menu, non working button

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,43 +1,59 @@
-import React from "react";
+import React, { Component } from "react";
 import PropTypes from "prop-types";
 
-function Header(props) {
-  return (
-    <nav className="navbar navbar-inverse navbar-fixed-top" role="navigation">
-      <div className="container">
-        <div className="navbar-header">
-          <button type="button" className="navbar-toggle" 
-            data-toggle="collapse" data-target="#bs-example-navbar-collapse-1">
-            <span className="sr-only">Toggle navigation</span>
-            <span className="icon-bar" />
-            <span className="icon-bar" />
-            <span className="icon-bar" />
-          </button>
-          <a className="navbar-brand" href="#">Start Bootstrap</a>
+
+export default class Header extends Component {
+  render() {
+    const checkOutBttn = this.props.cart.length !== 0 ? 
+      <button 
+        className="btn btn-primary btn-block pull-right" 
+        data-toggle="modal" 
+        data-target="#checkOut">
+        Proceed to Checkout
+      </button> : "Nothing in cart";
+
+    return (
+      <nav className="navbar navbar-inverse navbar-fixed-top" role="navigation">
+        <div className="container">
+          <div className="navbar-header">
+            <button type="button" className="navbar-toggle" 
+              data-toggle="collapse" data-target="#bs-example-navbar-collapse-1">
+              <span className="sr-only">Toggle navigation</span>
+              <span className="icon-bar" />
+              <span className="icon-bar" />
+              <span className="icon-bar" />
+            </button>
+            <a className="navbar-brand" href="#">Start Bootstrap</a>
+          </div>
+          <div className="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
+            <ul className="nav navbar-nav">
+              <li>
+                <a href="#">About</a>
+              </li>
+              <li>
+                <a href="#">Services</a>
+              </li>
+              <li>
+                <a href="#">Contact</a>
+              </li>
+              <li className="dropdown">
+                <a className="dropdown-toggle" data-toggle="dropdown" href="#">
+                  Items in Cart ({this.props.numberOfItemsInCart})
+                <span className="caret" /></a>
+                <ul className="dropdown-menu">
+                  <li><a href="#">{checkOutBttn}</a></li>
+                </ul>
+              </li>
+            </ul>
+          </div>
         </div>
-        <div className="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
-          <ul className="nav navbar-nav">
-            <li>
-              <a href="#">About</a>
-            </li>
-            <li>
-              <a href="#">Services</a>
-            </li>
-            <li>
-              <a href="#">Contact</a>
-            </li>
-            <li >
-              <a href="#">Items In Cart ({props.numberOfItemsInCart})</a>
-            </li>
-          </ul>
-        </div>
-      </div>
-    </nav>
-  );
+      </nav>
+    );
+  }
 }
 
 Header.propTypes = {
-  numberOfItemsInCart: PropTypes.number.isRequired
+  numberOfItemsInCart: PropTypes.number.isRequired,
+  cart: PropTypes.array.isRequired
 };
 
-export default Header;
