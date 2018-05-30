c8f51bc53c4eb19518b2dfc1f16454ea2e5bdd50
walzer85 <walzer85@gmail.com>
2018-05-29 19:23:18 -0500

    fixed the productdetail population.

--- src/components/ProductDetail.js
+++ src/components/ProductDetail.js
@@ -1,13 +1,29 @@
-import React from "react";
+import React from "react"
 
 function ProductDetail(props){
-    return({
-        price: "props.price",
-        name: "props.name",
-        description: "props.description",
-        reviews: "props.reviews",
-        stars: "props.stars"
-    });
+    return(
+        <div className="col-sm-4 col-lg-4 col-md-4">
+        <div className="thumbnail">
+            <img src={props.product.imgUrl} alt="" />
+            <div className="caption">
+                <h4 className="pull-right">{props.product.price}</h4>
+                <h4><a href="#">{props.product.name}</a>
+                </h4>
+                <p>{props.product.description} <a target="_blank" href="http://www.bootsnipp.com">Bootsnipp - http://bootsnipp.com</a>.</p>
+            </div>
+            <div className="ratings">
+                <p className="pull-right">{props.product.reviews} reviews</p>
+                <p>
+                    <span className="glyphicon glyphicon-star" />
+                    <span className="glyphicon glyphicon-star" />
+                    <span className="glyphicon glyphicon-star" />
+                    <span className="glyphicon glyphicon-star" />
+                    <span className="glyphicon glyphicon-star" />
+                </p>
+            </div>
+        </div>
+        </div>
+    );
 }
 
 export default ProductDetail;
